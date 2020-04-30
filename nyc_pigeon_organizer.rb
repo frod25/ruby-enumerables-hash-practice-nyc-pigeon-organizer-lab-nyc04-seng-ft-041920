a84409f429b1f3d3877f6468d66ed1aca15aa5d8
Felix Rodriguez <frod25@gmail.com>
2020-04-30 13:58:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,8 +24,10 @@ def nyc_pigeon_organizer(data)
   data.each_pair do |attributes, values|
     values.each_pair do |attribute, value|
       value.each do |bird|
-        if pigeons[bird] == nil || pigeons[bird] == {} || pigeons[bird][attributes] == nil
+        if pigeons[bird] == nil || pigeons[bird] == {}
           pigeons[bird] = {}
+          pigeons[bird][attributes] = []
+        elsif pigeons[bird][attributes] == nil
           pigeons[bird][attributes] = []
         end
       end
